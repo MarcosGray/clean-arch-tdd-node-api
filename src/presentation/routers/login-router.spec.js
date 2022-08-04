d7a9ebda0bafa06912641f8a050f0a719bbf2035
Marcos Gray <marcosgray@gmail.com>
2022-08-04 20:42:55 -0300

    Ensure LoginRouter returns 400 if no email no provider

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,23 @@
+class LoginRouter {
+    route(httpRequest){
+        const { email } = httpRequest.body
+        if (!email) {
+            return {
+                statusCode: 400
+            }
+        }
+    }
+}
+describe('Login Router', () => {
+    test('Should return 400 if no email is provider', () => {
+        // sut = system under test
+        const sut = new LoginRouter()
+        const httpRequest = {
+            body: {
+                password: 'any_password'
+            }
+        }
+        const httpResponse = sut.route(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
